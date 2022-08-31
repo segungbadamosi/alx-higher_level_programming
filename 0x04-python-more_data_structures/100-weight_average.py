2c689e1f9f561ac264fab7ab372b8d6f7cfc4d69
segungbadamosi <segun.gbadamosi1984@gmail.com>
2022-08-31 17:08:27 +0100

    Committing all the advanced tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    res = 0.0
+    res = 0
     s_list = list(t[0] * t[1] for t in my_list)
     w_list = list(t[1] for t in my_list)
     res = sum(s_list) / sum(w_list)
