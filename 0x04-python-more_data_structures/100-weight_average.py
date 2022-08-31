fd989af2c3b5624729e9908afda4c4258ea3812b
segungbadamosi <segun.gbadamosi1984@gmail.com>
2022-08-31 17:24:43 +0100

    Committing all the advanced tasks

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    weight = 0
-    product = []
     if len(my_list) == 0:
         return 0
-    for i in my_list:
-        weight += i[1]
-        product.append(i[0] * i[1])
-    return sum(product) / weight
+
+    return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
+
+
+def mul(x, y):
+    return x * y
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    new_list = []
-    for k, v in a_dictionary.items():
-        if value is v:
-            new_list.append(k)
-    if len(new_list) > 0:
-        for new in new_list:
-            del a_dictionary[new]
-    return a_dictionary
+ def complex_delete(a_dictionary, value):
+     keys = []
+     for x, y in a_dictionary.items():
+         if y == value:
+             keys.append(x)
+
+     for x in keys:
+         del a_dictionary[x]
+
+     return a_dictionary
