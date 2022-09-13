1b7c8e23b40b4ab29743b8fa6c1b48f1cc3238ac
segungbadamosi <segun.gbadamosi1984@gmail.com>
2022-09-13 22:44:20 +0100

    Classes

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    v = []
+    if value in a_dictionary.values():
+        for i in a_dictionary:
+            if a_dictionary[i] == value:
+                v += [i]
+        for j in v:
+            del a_dictionary[j]
+    return a_dictionary
+
+    #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
     for x in list(a_dictionary.keys()):
         if a_dictionary[x] is value:
             del a_dictionary[x]
